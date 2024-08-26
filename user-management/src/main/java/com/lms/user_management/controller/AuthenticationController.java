221497abc0a6527d6b35b21950a03a7f84134acc
Jinyyash <jinyyash@gmail.com>
2024-08-26 20:38:26 +0200

    setup logger

--- user-management/src/main/java/com/lms/user_management/controller/AuthenticationController.java
+++ user-management/src/main/java/com/lms/user_management/controller/AuthenticationController.java
@@ -6,6 +6,9 @@ import com.lms.user_management.input.RegisterUserInput;
 import com.lms.user_management.response.LoginResponse;
 import com.lms.user_management.service.AuthenticationService;
 import com.lms.user_management.service.JwtService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +21,8 @@ public class AuthenticationController {
     private final JwtService jwtService;
 
     private final AuthenticationService authenticationService;
+
+    Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
         this.jwtService = jwtService;
@@ -33,6 +38,7 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserInput loginUserDto) {
+        logger.info("User authenticate {}", loginUserDto.getEmail());
         User authenticatedUser = authenticationService.authenticate(loginUserDto);
 
         String jwtToken = jwtService.generateToken(authenticatedUser);
